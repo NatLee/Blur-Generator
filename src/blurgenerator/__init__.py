61e3dfa9764d94df70976cced752a88c2a264cc5
Nat Lee <natlee.work@gmail.com>
2023-02-01 17:13:56 +0800

    [update] update version

--- src/blurgenerator/__init__.py
+++ src/blurgenerator/__init__.py
@@ -1,10 +1,12 @@
 """
 Blur maker init
 """
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 from .motion_blur import motion_blur
 from .lens_blur import lens_blur
 from .gaussian_blur import gaussian_blur
+
+from .depth_mapping import blur_with_depth_layers
 
 from .cli import main
