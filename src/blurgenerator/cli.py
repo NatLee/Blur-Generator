b70a5646b90c399845db1a8be762ddfce8f9271e
Nat Lee <natlee.work@gmail.com>
2023-02-01 17:43:49 +0800

    [update] add temp

--- src/blurgenerator/cli.py
+++ src/blurgenerator/cli.py
@@ -107,3 +107,53 @@ def main():
     cv2.imwrite(args.output, result)
 
     return
+
+"""
+
+import numpy as np
+import cv2
+
+from blurgenerator import lens_blur, gaussian_blur
+
+def map_range(value, inMin, inMax, outMin, outMax):
+    return outMin + (((value - inMin) / (inMax - inMin)) * (outMax - outMin))
+
+def blur_with_depth(img, depth, num_layers=10, min_blur=1, max_blur=100):
+    min_depth = np.min(np.unique(depth))
+    max_depth = np.max(np.unique(depth))
+    step = (max_depth - min_depth) // num_layers
+    layers = np.array(range(min_depth, max_depth, step))
+    out = np.zeros(img.shape)
+
+    for idx, value in enumerate(layers):
+        dm = cv2.cvtColor(depth, cv2.COLOR_BGR2GRAY)
+        m = np.zeros(dm.shape)
+        m[dm > value] = 255
+        m[dm > (value + step)] = 0
+
+        l_mask = depth.copy()
+        l_mask[:,:,0] = m[:,:]
+        l_mask[:,:,1] = m[:,:]
+        l_mask[:,:,2] = m[:,:]
+
+        blur_amount = int(map_range(value, 0, 255, min_blur, max_blur))
+        #slice = gaussian_blur(img, blur_amount)
+        slice = lens_blur(img/255., radius=blur_amount)
+
+        _, mask = cv2.threshold(l_mask, 100, 255, cv2.THRESH_BINARY)
+        mask_inv = cv2.bitwise_not(mask)
+        layer = cv2.bitwise_and(slice, slice, mask = mask[:,:,0])
+        out = cv2.add(out, layer, dtype=0)
+
+    h,w,c = out.shape
+    ha = h*2 // 3
+    wa = w*2 // 3
+    out = cv2.resize(out, (wa,ha))
+    return out
+
+img = cv2.imread("input.jpg")
+depth = cv2.imread("depth.png")
+output = blur_with_depth(img, depth, num_layers=3, min_blur=1, max_blur=50)
+cv2.imwrite('output.jpg', output)
+
+"""
